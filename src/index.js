5e3c414aeb0b4ffe14eb1ef152b52e5670a88187
Rasmus Ståhl <rasmus.stahl.47@gmail.com>
2020-04-18 21:47:32 +0200

    tidy public dir

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import MainPage from './components/MainPage';
 
-// require('dotenv').config();
-
-//console.log(process.env);
-
 ReactDOM.render(<MainPage />, document.getElementById('root'));
 
 serviceWorker.unregister();
